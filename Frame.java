cf572adced3732703feaaf48ef637d2280a2a7d1
ManivanzM <119950529+ManivanzM@users.noreply.github.com>
2023-04-18 13:27:50 +0530

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,12 @@
+package com.company;
+import javax.swing.JFrame;
+public class Frame extends JFrame{
+
+    Frame(){
+        this.setTitle("SnakeGame");
+        this.add(new Panel());
+        this.setVisible(true);
+        this.setResizable(false);
+        this.pack();
+    }
+}
